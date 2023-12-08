65bae07ea4dc05edfbba242a07206146738230f1
Faizulafnaz <faizulafnaz22@gmail.com>
2023-12-08 16:44:33 +0530

    first_commit

---
+++ authentication/urls.py
@@ -0,0 +1,12 @@
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+from .views import MyTokenObtainPairView, UserRegister
+from django.urls import path
+
+
+urlpatterns = [
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('signup/', UserRegister.as_view(), name='signup')
+
+]
