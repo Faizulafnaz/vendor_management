8003d19f2f324e3d8875f50ba9fcf4d59e78defb
Faizulafnaz <faizulafnaz22@gmail.com>
2023-12-08 16:52:44 +0530

    requitements added

--- authentication/urls.py
+++ authentication/urls.py
@@ -8,5 +8,4 @@ from django.urls import path
 urlpatterns = [
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('signup/', UserRegister.as_view(), name='signup')
-
 ]
