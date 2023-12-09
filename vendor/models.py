21187cfb9f917249c5802871a3c36762691dd9e0
Faizulafnaz <faizulafnaz22@gmail.com>
2023-12-09 12:39:04 +0530

    authentication bug fixed

--- vendor/models.py
+++ vendor/models.py
@@ -54,7 +54,7 @@ class PurchaseOrder(models.Model):
 
 class Performance(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True)
     on_time_delivery_rate = models.FloatField(default=0)
     quality_rating_avg = models.FloatField(default=0)
     average_response_time = models.FloatField(default=0)
